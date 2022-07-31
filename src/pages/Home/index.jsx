a4efd3312016d6d7d31d0b25234803816fdbb41a
Manav <mjfx102@gmail.com>
2022-07-31 22:13:27 +0530

    fourth commit

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import FilterPanel from "../../components/Home/FilterPanel";
 import List from "../../components/Home/List";
 import NavBar from "../../components/Home/NavBar";
@@ -10,6 +10,7 @@ const Home = () => {
    const [selectedRating , setSelectedRating] = useState(null);
    const [selectedPrice , setSelectedPrice] = useState([500,5000]);
    const [list , setList] = useState(dataList);
+   const [inputSearch , setInputSearch] = useState('');
    const [brands , setBrands] = useState([
     {
       id:1,
@@ -35,10 +36,41 @@ const Home = () => {
     setBrands(changeCheckedBrands);
   };
   const handleChangePrice=(event,value)=> setSelectedPrice(value);
+  const applyFilters = () => {
+    let updatedList = dataList;
+    if(selectedRating){
+      updatedList = updatedList.filter(
+        (item) => parseInt(item.rating) === parseInt(selectedRating)
+      );
+    }
+    if(selectedCategory){
+      updatedList=updatedList.filter(
+        (item)=> item.category === selectedCategory
+      );
+    }
+    const brandChecked = brands.filter(item=>item.checked)
+    .map((item) => item.label);
+    if(brandChecked.length){
+      updatedList=updatedList.filter((item)=>brandChecked.includes(item.brand))
+    }
+    const minPrice = selectedPrice[0];
+    const maxPrice = selectedPrice[1];
+    updatedList=updatedList.filter((item)=>item.price>=minPrice && item.price<=maxPrice)
+    if(inputSearch){
+      updatedList = updatedList.filter((item)=>
+      item.title.search(inputSearch.trim()) !==
+      -1
+      );
+    }
+    setList(updatedList);
+  };
+  useEffect(()=>{
+    applyFilters();
+  },[selectedRating,selectedCategory,brands,selectedPrice,inputSearch])
   return (
     <div className="home">
       {/*Nav Bar */}
-      <NavBar />
+      <NavBar value={inputSearch} changeInput = {event=>setInputSearch(event.target.value)} />
       <div className="home_panelList-wrap">
         <div className="home_panel-wrap">
           {/*Side Panels */}
